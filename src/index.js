4f0a1f96731e5c2619acd6a799fa249cb8128641
anyoneout <chrisdafur@gmail.com>
2025-01-14 02:38:27 -0600

    set up BrowserRouter and added route structure to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+
 
 const root = createRoot(window.bodyTag);
 root.render(
-  <></>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/OaPage" element={<OaPage />} />
+      <Route path="/BfPage" element={<BfPage />} />
+      <Route path="/About" element={<About />} />
+    </Routes>
+  </BrowserRouter>
 );   
