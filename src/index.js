7b26893b16be086e85532ab1d94dcbdefc43fd09
anyoneout <chrisdafur@gmail.com>
2025-01-14 23:22:21 -0600

    added basename=React-Capstone-with-branches to ensure that img root paths are handled corretly

--- src/index.js
+++ src/index.js
@@ -12,13 +12,14 @@ import { OaPage } from "./Views/OaPage";
 
 const root = createRoot(window.bodyTag);
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/React-Capstone-with-branches">
     <Nav />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/OaPage" element={<OaPage />} />
       <Route path="/BfPage" element={<BfPage />} />
       <Route path="/About" element={<About />} />
+
     </Routes>
   </BrowserRouter>
 );   
