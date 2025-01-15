253dfdabc337e0be71d339648f44dbad63aa5702
anyoneout <chrisdafur@gmail.com>
2025-01-14 18:21:59 -0600

    added images and imports to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { Home } from "./Views/Home";
+import { Nav } from "./Views/Nav";
+import { About } from "./Views/About";
+import { BfPage } from "./Views/BfPage";
+import { OaPage } from "./Views/OaPage";
+
+
 
 
 const root = createRoot(window.bodyTag);
 root.render(
   <BrowserRouter>
+    <Nav />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/OaPage" element={<OaPage />} />
