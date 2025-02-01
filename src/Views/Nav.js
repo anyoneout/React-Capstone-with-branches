e47376fded90019df4e22d04337314ad2d4be8e9
anyoneout <chrisdafur@gmail.com>
2025-02-01 10:57:07 -0600

    added nav and nav-link to Nav.js

--- src/Views/Nav.js
+++ src/Views/Nav.js
@@ -4,7 +4,7 @@ import { Link, NavLink } from "react-router";
 export function Nav() {
   const domain = window.location.hostname;
   let rootPath = "";
-  if (domain === "anyoneout.github.io") rootPath = "/react-project";
+  if (domain === "anyoneout.github.io") rootPath = "/React-Capstone-with-branches";
   return (
     <>
       <div className="container mt-3">
@@ -13,18 +13,18 @@ export function Nav() {
           <div className="col-4">
             <div className="d-flex justify-content-evenly align-items-end" >
               <NavLink to={`${rootPath}/`} style={{ fontSize: "13px" }}
-                className="link-underline-opacity-0 link-underline text-info">
+                className="nav nav-link link-underline-opacity-0 link-underline text-info">
                 Home
               </NavLink>|
               <NavLink to={`${rootPath}/BfPage`} style={{ fontSize: "13px" }}
-                className="link-underline-opacity-0 link-underline text-info">Black Forest
+                className="nav nav-link link-underline-opacity-0 link-underline text-info">Black Forest
               </NavLink>|
               <NavLink to={`${rootPath}/OaPage`}
                 style={{ fontSize: "13px" }}
-                className="link-underline-opacity-0 link-underline text-info">Dall-E-3
+                className="nav nav-link link-underline-opacity-0 link-underline text-info">Dall-E-3
               </NavLink>|
               <NavLink to={`${rootPath}/About`} style={{ fontSize: "13px" }}
-                className="link-underline-opacity-0 link-underline text-info">
+                className="nav nav-link link-underline-opacity-0 link-underline text-info">
                 About
               </NavLink>
             </div>
