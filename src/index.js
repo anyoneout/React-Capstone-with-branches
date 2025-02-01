645276c901b0a7dec730b2a9e819bcc4c2ad87ef
anyoneout <chrisdafur@gmail.com>
2025-01-31 22:55:47 -0600

    changed webpack.config.json to a .js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { Home } from "./Views/Home";
 import { Nav } from "./Views/Nav";
 import { About } from "./Views/About";
@@ -12,13 +12,14 @@ import { OaPage } from "./Views/OaPage";
 
 const root = createRoot(window.bodyTag);
 root.render(
-  <BrowserRouter basename="/React-Capstone-with-branches/">
+  <BrowserRouter>
     <Nav />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/OaPage" element={<OaPage />} />
       <Route path="/BfPage" element={<BfPage />} />
       <Route path="/About" element={<About />} />
+      <Route path="*" element={<Navigate to="/" />} />
 
     </Routes>
   </BrowserRouter>
