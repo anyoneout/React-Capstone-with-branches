806f97f37328b1cc1c739c19d4156f69a697479f
anyoneout <chrisdafur@gmail.com>
2025-02-01 15:46:13 -0600

    removed unnecessary spacing columns from Nav and changed car size to 90%

--- src/Views/Nav.js
+++ src/Views/Nav.js
@@ -6,30 +6,26 @@ export function Nav() {
   let rootPath = "";
   if (domain === "anyoneout.github.io") rootPath = "/React-Capstone-with-branches";
   return (
-    <nav className="nav nav-pills">
-      <div className="container mt-3">
-        <div className="row">
-          <div className="col-4"></div>
-          <div className="col-4">
-            <div className="d-flex justify-content-evenly align-items-end" >
-              <NavLink to={`${rootPath}/`} style={{ fontSize: "13px" }}
-                className="nav-link text-info">
-                Home
-              </NavLink>
-              <NavLink to={`${rootPath}/BfPage`} style={{ fontSize: "13px" }}
-                className="nav-link text-info">Black Forest
-              </NavLink>
-              <NavLink to={`${rootPath}/OaPage`}
-                style={{ fontSize: "13px" }}
-                className="nav-link text-info">Dall-E-3
-              </NavLink>
-              <NavLink to={`${rootPath}/About`} style={{ fontSize: "13px" }}
-                className="nav-link text-info">
-                About
-              </NavLink>
-            </div>
-          </div>
-          <div className="col-4"></div>
+    <nav className="nav nav-pills fixed=top">
+      <div className="container">
+
+        <div className="d-flex justify-content-center" >
+          <NavLink to={`${rootPath}/`} style={{ fontSize: "13px" }}
+            className="nav-link text-info">
+            Home
+          </NavLink>
+          <NavLink to={`${rootPath}/BfPage`} style={{ fontSize: "13px" }}
+            className="nav-link text-info">Black Forest
+          </NavLink>
+          <NavLink to={`${rootPath}/OaPage`}
+            style={{ fontSize: "13px" }}
+            className="nav-link text-info">Dall-E-3
+          </NavLink>
+          <NavLink to={`${rootPath}/About`} style={{ fontSize: "13px" }}
+            className="nav-link text-info">
+            About
+          </NavLink>
+
         </div>
       </div>
     </nav>
