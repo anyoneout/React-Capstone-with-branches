e736666e19f0c40650bcb9a5e8ede084d8c0c416
anyoneout <chrisdafur@gmail.com>
2025-02-05 17:46:22 -0600

    Added tracked mount phases and title change

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HomeGenerator } from "../components/HomeGenerator";
 
 
 export function Home() {
+
+  const [didMount, setDidMount] = useState(false);
+
+  useEffect(componentDidMount, []);
+  useEffect(componentDidUpdate, [didMount]);
+  useEffect(componentDidUnmount, []);
+
   return <HomeGenerator />
+
+  function componentDidMount() {
+    setDidMount(true);
+    console.log("The component has mounted");
+    document.title = "Recipe Deconstructor - Home";
+    return componentDidUnmount;
+  }
+
+  function componentDidUpdate() {
+    if (didMount) console.log("The component has updated");
+  }
+
+  function componentDidUnmount() {
+    function delayedUnmount() {
+      console.log("The component has unmounted");
+    }
+    return delayedUnmount;
+  }
 }
