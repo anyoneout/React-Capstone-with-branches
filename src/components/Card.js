7be18e3d853a1dc3beed66dd31bd5b395b52e37a
anyoneout <chrisdafur@gmail.com>
2025-02-06 23:56:41 -0600

    added tooltips to Home.js images and modified Card.js to use custom css

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,6 +7,11 @@ export function Card(props) {
       style={{ width: "90%", height: "auto", backgroundColor: "black" }}
     >
       <img
+        data-bs-custom-class="custom-tooltip"
+        data-bs-placement={props.tooltipPlacement}
+        data-bs-title={props.tooltipTitle}
+        data-bs-toggle="tooltip"
+        id={props.id}
         src={props.src}
         className="card-img img-fluid home-image"
         alt="..."
