a86d6ca0f199e2314b5a4a8db6e9d23ba90a93b4
anyoneout <chrisdafur@gmail.com>
2025-02-11 00:31:07 -0600

    added new images and Examples.js page. Set up Carousel to have two carousels which show the same images on an interval for both models

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,34 +1,122 @@
 import React from "react";
-import oaBsRecipe from "../img/openaibsrecipe.jpg";
-import oaBsIngredients from "../img/openaibsingredients.jpg";
-import oaCsRecipe from "../img/openaicsrecipe.jpg";
-import oaCsIngredients from "../img/openaicsingredients.jpg";
+import oaBananaSplitR from "../img/OpenAiImages/oaBananaSplitR.jpg";
+import oaBananaSplitI from "../img/OpenAiImages/oaBananaSplitI.jpg";
+import oaCapreseSaladR from "../img/OpenAiImages/oaCapreseSaladR.jpg";
+import oaCapreseSaladI from "../img/OpenAiImages/oaCapreseSaladI.jpg";
+import oaCaesarSaladR from "../img/OpenAiImages/oaCaesarSaladR.png";
+import oaCaesarSaladI from "../img/OpenAiImages/oaCaesarSaladI.png";
+import oaBakedAlaskaR from "../img/OpenAiImages/oaBakedAlaskaR.png";
+import oaBakedAlaskaI from "../img/OpenAiImages/oaBakedAlaskaI.png";
+import bfBananaSplitR from "../img/BlackForestImages/bfBananaSplitR.jpg";
+import bfBananaSplitI from "../img/BlackForestImages/bfBananaSplitI.jpg";
+import bfCapreseSaladR from "../img/BlackForestImages/bfCapreseSaladR.jpg";
+import bfCapreseSaladI from "../img/BlackForestImages/bfCapreseSaladI.jpg";
 
 export default function Carousel() {
   return (
-    <div id="carouselExample" className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src={oaBsRecipe} className="d-block w-100" alt="..." />
+    <>
+      <CarouselOpenAi />
+      <CarouselBlackForest />
+    </>
+  )
+}
+
+export function CarouselOpenAi() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <div id="carouselRecipes" className="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={oaBananaSplitR} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={oaCapreseSaladR} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={oaCaesarSaladR} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={oaBakedAlaskaR} className="carousel-images" alt="..." />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="carousel-item">
-          <img src={oaBsIngredients} className="d-block w-100" alt="..." />
-        </div>
-        <div className="carousel-item active">
-          <img src={oaCsRecipe} className="d-block w-100" alt="..." />
-        </div>
-        <div className="carousel-item">
-          <img src={oaCsIngredients} className="d-block w-100" alt="..." />
+        <div className="col-6">
+          <div id="carouselIngredients" className="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={oaBananaSplitI} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={oaCapreseSaladI} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={oaCaesarSaladI} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={oaBakedAlaskaI} className="carousel-images" alt="..." />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    </div>
+
+    /*   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Previous</span>
       </button>
       <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
         <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Next</span>
-      </button>
+      </button> */
+
+  )
+}
+
+export function CarouselBlackForest() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <div id="carouselBfRecipes" className="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={bfBananaSplitR} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={bfCapreseSaladR} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={oaCaesarSaladR} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={oaBakedAlaskaR} className="carousel-images" alt="..." />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <div id="carouselIngredients" className="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={bfBananaSplitI} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={bfCapreseSaladI} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={oaCaesarSaladI} className="carousel-images" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={oaBakedAlaskaI} className="carousel-images" alt="..." />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-
 import { Home } from "./Views/Home";
 import { About } from "./Views/About";
 import { BfPage } from "./Views/BfPage";
 import { OaPage } from "./Views/OaPage";
 import { CollapsibleNavbar } from "./components/CollapsibleNavbar";
-import "../style.scss";
 import SignInForm from "./Views/SignInForm";
-
-
+import Examples from "./Views/Examples";
+import "../style.scss";
 
 const bodyTag = document.getElementById("bodyTag");
 const root = createRoot(bodyTag);
@@ -24,6 +22,7 @@ root.render(
       <Route path={`${rootPath}/`} element={<Home />} />
       <Route path={`${rootPath}/OaPage`} element={<OaPage />} />
       <Route path={`${rootPath}/BfPage`} element={<BfPage />} />
+      <Route path={`${rootPath}/Examples`} element={<Examples />} />
       <Route path={`${rootPath}/About`} element={<About />} />
       <Route path={`${rootPath}/SignInForm`} element={<SignInForm />} />
       <Route path="*" element={<Navigate to="/" />} />
